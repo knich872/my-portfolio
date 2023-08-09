60c9816359a54ac324b7415e3ebd7635fd6e6c99
Kyle Nichols <kyle.r.nichols@gmail.com>
2023-08-09 13:20:06 +0900

    routes, controller, views added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   root to: "pages#home"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get "pages/about", to: "pages#about"
+  get "pages/projects", to: "pages#projects"
+  get "pages/contact", to: "pages#contact"
   # Defines the root path route ("/")
   # root "articles#index"
 end
